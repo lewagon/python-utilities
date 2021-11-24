bce46c764974b1921fbd3655ba62cee73054cdb6
Gaetan Manchon <gmanchon@gmail.com>
2021-11-24 15:01:32 +0100

    correct version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     requirements = [c.strip() for c in f.readlines()]
 
 setup(name="wagon_common",
-      version="0.3.1",
+      version="0.2.2",
       description="Le Wagon common packages",
       url="https://github.com/lewagon/python-utilities/",
       author="Sébastien Saunier",
