edcbb3e2fdc9ce6f8f224c303545328d3b273ac4
Gaetan Manchon <gmanchon@gmail.com>
2022-04-01 10:27:05 +0200

    add tests for repo to url

--- tests/helpers/gh/test_url.py
+++ tests/helpers/gh/test_url.py
@@ -17,3 +17,21 @@ class TestGitHubRepo():
         assert data_cha.repo == "data-challenges"
 
         # Cleanup
+
+    def test_repo_to_url(self):
+
+        # Arrange
+        url = "https://github.com/lewagon/data-challenges.git"
+
+        # Act
+        data_cha = GitHubRepo.from_url(url)
+        repo_url = data_cha.to_url()
+        repo_clone_url = data_cha.to_clone_url()
+        repo_clone_ssh = data_cha.to_clone_ssh()
+
+        # Assert
+        assert repo_url == "https://github.com/lewagon/data-challenges"
+        assert repo_clone_url == "https://github.com/lewagon/data-challenges.git"
+        assert repo_clone_ssh == "git@github.com:lewagon/data-challenges.git"
+
+        # Cleanup
