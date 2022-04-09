3948eba3b98102d2aaf5851f423c483d28d15836
Gaetan Manchon <gmanchon@gmail.com>
2022-04-09 13:50:55 +0200

    add option to git tld helper to run from any path + tests

--- wagon_common/helpers/git/repo.py
+++ wagon_common/helpers/git/repo.py
@@ -4,18 +4,31 @@ git repo cli helpers
 
 from wagon_common.helpers.subprocess import run_command
 
+import os
 
-def get_git_top_level_directory(verbose=False):
+
+def get_git_top_level_directory(verbose=False, path=None):
     """ retrieve the top level of the git repository """
 
-    # getting git repo top level
+    # check path
+    if path and not os.path.isdir(path):
+        path = os.path.dirname(os.path.abspath(path))
+
+        # verify path
+        if not os.path.isdir(path):
+            return None
+
+    # get git repo top level directory
     command = [
         "git",
         "rev-parse",
         "--show-toplevel",
         ]
 
-    _rc, output, _error = run_command(command, verbose=verbose)
+    rc, output, _error = run_command(command, cwd=path, verbose=verbose)
+
+    if rc != 0:
+        return None
 
     if verbose:
         print(output.decode("utf-8"))
@@ -28,6 +41,12 @@ def get_git_top_level_directory(verbose=False):
 
 if __name__ == '__main__':
 
+    data_challenges_inner_path = os.path.normpath(os.path.join(
+        os.path.dirname(__file__), "..", "..", "..", "..",
+        "data-challenges", "00-Setup", "01-Check", "tests", "test_demo.py"))
+
     # test tld
-    tld = get_git_top_level_directory()
-    print(f"top level directory: >{tld}<")
+    tld = get_git_top_level_directory(
+        path=data_challenges_inner_path, verbose=True)
+
+    print(f"data challenges tld: {tld}")
