ab9ef61b44f3ff233ab40c5da3b5fbd825c05c5c
Gaetan Manchon <gmanchon@gmail.com>
2022-04-18 08:46:58 +0200

    correct test

--- tests/cmd/test_publication_command.py
+++ tests/cmd/test_publication_command.py
@@ -5,6 +5,7 @@ from wagon_common.path.scope import Scope
 from wagon_common.tests.base.directory_equality import TestBaseDirectoryEquality
 
 import os
+import glob
 
 
 class TestPublicationCommand(TestBaseDirectoryEquality):
@@ -23,9 +24,13 @@ class TestPublicationCommand(TestBaseDirectoryEquality):
         # Act
         def act():
 
-            scope = Scope.from_sources([
+            sources = [
                 os.path.join(self.source_root, "07", "04", "01", "in_scope.py"),
-                os.path.join(self.source_root, "04", "*.py")])
+                os.path.join(self.source_root, "04", "**", "*.py")]
+
+            globbed = [r for s in sources for r in glob.glob(s, recursive=True)]
+
+            scope = Scope.from_sources(globbed)
 
             PublicationCommand().run(
                 scope=scope,
@@ -34,84 +39,5 @@ class TestPublicationCommand(TestBaseDirectoryEquality):
         # Assert
         self.run_test_directory_identical(
             os.path.join(self.tests_root, "publish"), act)
-
-        # Cleanup
-
-    def test_publication_command_subpub(self):
-        """
-        source: /path/project/  command/root/   content/path
-        target: /path/target/   target/root/    content/path
-        """
-
-        # Arrange
-
-        # Act
-        def act():
-
-            scope = Scope.from_sources([
-                os.path.join(self.source_root, "07", "04", "01", "in_scope.py"),
-                os.path.join(self.source_root, "04", "*.py")])
-
-            PublicationCommand().run(
-                scope=scope,
-                target_tld=self.processed_root,
-                command_root=os.path.join("command", "root"),
-                target_root=os.path.join("target", "root"))
-
-        # Assert
-        self.run_test_directory_identical(
-            os.path.join(self.tests_root, "subpub"), act)
-
-        # Cleanup
-
-    def test_publication_command_command_root(self):
-        """
-        source: /path/project/  command/root/   content/path
-        target: /path/target/   target/root/    content/path
-        """
-
-        # Arrange
-
-        # Act
-        def act():
-
-            scope = Scope.from_sources([
-                os.path.join(self.source_root, "07", "04", "01", "in_scope.py"),
-                os.path.join(self.source_root, "04", "*.py")])
-
-            PublicationCommand().run(
-                scope=scope,
-                target_tld=self.processed_root,
-                command_root=os.path.join("command", "root"))
-
-        # Assert
-        self.run_test_directory_identical(
-            os.path.join(self.tests_root, "command"), act)
-
-        # Cleanup
-
-    def test_publication_command_target_root(self):
-        """
-        source: /path/project/  command/root/   content/path
-        target: /path/target/   target/root/    content/path
-        """
-
-        # Arrange
-
-        # Act
-        def act():
-
-            scope = Scope.from_sources([
-                os.path.join(self.source_root, "07", "04", "01", "in_scope.py"),
-                os.path.join(self.source_root, "04", "*.py")])
-
-            PublicationCommand().run(
-                scope=scope,
-                target_tld=self.processed_root,
-                target_root=os.path.join("target", "root"))
-
-        # Assert
-        self.run_test_directory_identical(
-            os.path.join(self.tests_root, "target"), act)
 
         # Cleanup
