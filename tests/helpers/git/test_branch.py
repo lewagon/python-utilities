d550c5ada9a54315301f8d945b6190f11b7dac06
Gaëtan Manchon <34584935+gmanchon@users.noreply.github.com>
2022-10-21 03:51:00 +0200

    Update tests/helpers/git/test_branch.py

--- tests/helpers/git/test_branch.py
+++ tests/helpers/git/test_branch.py
@@ -3,7 +3,9 @@ from wagon_common.helpers.git.branch import rename_branch, get_current_branch
 class TestGitBranchHelper():
 
     def test_git_branch_renamed(self):
+
       very_fancy_branch_name = 'very-fancy-branch-3000'
+
       # get branch name with get_branch_name
       rc, _output, _error, original_branch = get_current_branch(None)
       assert rc == 0
