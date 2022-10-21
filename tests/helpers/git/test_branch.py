3588c75d3726b8aaae40071d8a7ddd891bb844d6
Gaëtan Manchon <34584935+gmanchon@users.noreply.github.com>
2022-10-21 04:33:28 +0200

    Update tests/helpers/git/test_branch.py

--- tests/helpers/git/test_branch.py
+++ tests/helpers/git/test_branch.py
@@ -17,7 +17,6 @@ class TestGitBranchHelper():
 
         # rename it to 'very-fancy-branch'
         rc, _output, _error = rename_branch(None, very_fancy_branch_name)
-
         assert rc == 0
 
         # get_branch_name to check it matches above
