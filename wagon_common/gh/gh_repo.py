955869dfb20414d1117b11c43921f8351b4fbb70
Gaetan Manchon <gmanchon@gmail.com>
2022-11-14 18:40:35 +0100

    add gh repo delete method

--- wagon_common/gh/gh_repo.py
+++ wagon_common/gh/gh_repo.py
@@ -1,13 +1,20 @@
 
+import requests
+
+from wagon_common.helpers.output import red
+
+
 class GhRepo:
     """
     helper class for gh api commands
     """
 
-    def __init__(self, name, is_org=True, verbose=False):
+    def __init__(self, name, token=None, is_org=True, verbose=False):
 
         self.is_org = is_org
         self.name, self.owner, self.repository = self.__identify(name)
+        self.base_url = f"https://api.github.com/repos/{self.owner}/{self.repository}"
+        self.token = token
         self.verbose = verbose
 
     def __identify(self, name):
@@ -28,6 +35,38 @@ class GhRepo:
 
         return name, owner, repository
 
+    def __call(self, path="", verb="get", headers={}, params={}, context=""):
+        """
+        resolve api call
+        """
+
+        # resolve http verb call method
+        call_method = dict(
+            get=requests.get,
+            put=requests.put,
+            patch=requests.patch,
+            post=requests.post,
+            delete=requests.delete)[verb]
+
+        # add auth
+        headers["Authorization"] = f"token {self.token}"
+
+        # list repo params
+        response = call_method(self.base_url + path,
+                               headers=headers,
+                               json=params)
+
+        if response.status_code != 200:
+
+            red("\nGH api error 🤕",
+                f"\n- context {context}"
+                + f"\n- status code: {response.status_code}"
+                + f"\n- response: {response.content}")
+
+            raise ValueError("GH api error")
+
+        return response.json()
+
     def delete(self):
         """
         delete repository
@@ -36,3 +75,10 @@ class GhRepo:
         # protect production repositories
         if self.owner not in ["lewagon-test", "Le-Wagon-QA"]:
             raise NameError("cannot delete repo in production organisation")
+
+        # delete repo
+        params = dict(
+            owner=self.owner,
+            repo=self.repository)
+
+        self.__call(verb="delete", params=params)
