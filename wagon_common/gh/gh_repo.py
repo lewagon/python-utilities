baed2b57d6eb57f37283ca4d84ab942eeeced2a4
Gaetan Manchon <gmanchon@gmail.com>
2022-11-16 18:53:01 +0100

    increase timeout

--- wagon_common/gh/gh_repo.py
+++ wagon_common/gh/gh_repo.py
@@ -154,7 +154,7 @@ class GhRepo:
         repo = None
 
         index = 0
-        max_tries = 12  # 1 minute
+        max_tries = 12 * 3  # 3 minutes
 
         green(f"\nWait for the creation of the {self.name} repo")
 
