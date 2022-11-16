ff4603ea3e291443a371d2937059ed40985d4b01
Gaetan Manchon <gmanchon@gmail.com>
2022-11-16 21:09:02 +0100

    revert default branch

--- wagon_common/git/git_repo.py
+++ wagon_common/git/git_repo.py
@@ -59,7 +59,7 @@ class GitRepo:
                 ".",
             ] + (["--quiet"] if quiet else []))
 
-    def init(self, initial_branch="main"):
+    def init(self, initial_branch="master"):
 
         return self.__command(
             "Initialize git repo",
