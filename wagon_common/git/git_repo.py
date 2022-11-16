dea31698de1138cca406df0957d791e2321ade52
Gaetan Manchon <gmanchon@gmail.com>
2022-11-16 23:49:13 +0100

    add git user config

--- wagon_common/git/git_repo.py
+++ wagon_common/git/git_repo.py
@@ -59,6 +59,28 @@ class GitRepo:
                 ".",
             ] + (["--quiet"] if quiet else []))
 
+    def config_user_name(self, name: str = "github-actions"):
+
+        return self.__command(
+            "Config git user name",
+            [
+                "git",
+                "config",
+                "user.email",
+                name
+            ])
+
+    def config_user_email(self, email: str = "github-actions@github.com"):
+
+        return self.__command(
+            "Config git user email",
+            [
+                "git",
+                "config",
+                "user.email",
+                email
+            ])
+
     def init(self, initial_branch="master"):
 
         return self.__command(
