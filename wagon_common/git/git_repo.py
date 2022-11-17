7a8468631435c819c97f24b013cb3ddfbc029779
Gaetan Manchon <gmanchon@gmail.com>
2022-11-17 12:13:13 +0100

    correct git config command

--- wagon_common/git/git_repo.py
+++ wagon_common/git/git_repo.py
@@ -66,7 +66,7 @@ class GitRepo:
             [
                 "git",
                 "config",
-                "user.email",
+                "user.name",
                 name
             ])
 
